8e330d88ca1487cf780fb2e23256b3b41a76a05e
Laurens Van Houtven <_@lvh.io>
2016-01-10 10:20:11 -0600

    Add cljfmt plugin

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.cloudbees/syslog-java-client "1.0.7"]]
+  :plugins [[lein-cljfmt "0.3.0"]]
   :main ^:skip-aot unclogged.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
