433f9abdb0040502b6fd72b40ae7298a43c76795
Laurens Van Houtven <_@lvh.io>
2016-01-09 18:32:44 -0800

    I forgot how much stuff syslog had

--- test/unclogged/core_test.clj
+++ test/unclogged/core_test.clj
@@ -5,7 +5,37 @@
   (:import
    [com.cloudbees.syslog Facility]))
 
+(defn facility-bit-flag
+  "In syslog, facilities don't use the lowest 2 bits; they're ints
+  shifted left by 3."
+  [n]
+  (bit-shift-left n 3))
+
 (deftest facility-tests
   (testing "numerical codes"
-    (are [code facility] (= (@#'c/facility code) facility)
-      0 Facility/KERN)))
+    (are [code facility] (= (@#'unclogged.core/facility (facility-bit-flag code))
+                            facility)
+      0 Facility/KERN
+      1 Facility/USER
+      2 Facility/MAIL
+      3 Facility/DAEMON
+      4 Facility/AUTH
+      5 Facility/SYSLOG
+      6 Facility/LPR
+      7 Facility/NEWS
+      8 Facility/UUCP
+      9 Facility/CRON
+      10 Facility/AUTHPRIV
+      11 Facility/FTP
+      12 Facility/NTP
+      13 Facility/AUDIT
+      14 Facility/ALERT
+      15 Facility/CLOCK
+      16 Facility/LOCAL0
+      17 Facility/LOCAL1
+      18 Facility/LOCAL2
+      19 Facility/LOCAL3
+      20 Facility/LOCAL4
+      21 Facility/LOCAL5
+      22 Facility/LOCAL6
+      23 Facility/LOCAL7)))
