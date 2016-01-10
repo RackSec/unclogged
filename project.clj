f155b9b7ac009d4dc877aed8a2ac9cf2ae4f0d8c
Laurens Van Houtven <_@lvh.io>
2016-01-09 18:20:26 -0800

    Add stub project.clj

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject unclogged "0.1.0-SNAPSHOT"
+  :description "Clojure syslog abstraction"
+  :url "https://github.com/RackSec/unclogged"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.cloudbees/syslog-java-client "1.0.7"]]
+  :main ^:skip-aot unclogged.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
