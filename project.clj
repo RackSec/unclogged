e1cb2b9ec98b24e94c1c091477265e8f50a37b9d
Laurens Van Houtven <_@lvh.io>
2016-01-13 11:14:17 -0600

    Add manifold, timbre tests

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0-RC5"]
-                 [com.cloudbees/syslog-java-client "1.0.8"]]
+                 [com.cloudbees/syslog-java-client "1.0.8"]
+                 [manifold "0.1.2-alpha2"]
+                 [com.taoensso/timbre "4.2.0"]]
   :plugins [[lein-cljfmt "0.3.0"]]
   :main ^:skip-aot unclogged.core
   :target-path "target/%s"
