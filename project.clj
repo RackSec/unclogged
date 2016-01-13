250b06d5edafeecbe25685745bc3d737aec93240
Laurens Van Houtven <_@lvh.io>
2016-01-13 11:02:12 -0600

    Upgrade syslog library

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0-RC5"]
-                 [com.cloudbees/syslog-java-client "1.0.7"]]
+                 [com.cloudbees/syslog-java-client "1.0.8"]]
   :plugins [[lein-cljfmt "0.3.0"]]
   :main ^:skip-aot unclogged.core
   :target-path "target/%s"
