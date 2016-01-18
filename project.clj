01ebf0f9481783fcce28cf48564a44ce4e46f234
Laurens Van Houtven <_@lvh.io>
2016-01-18 16:35:45 -0600

    Version 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject unclogged "0.2.1-SNAPSHOT"
+(defproject unclogged "0.2.1"
   :description "Clojure syslog abstraction"
   :url "https://github.com/RackSec/unclogged"
   :license {:name "Eclipse Public License"
