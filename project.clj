d9983b59468ab0f0c039366bb16eefe840a3cf2d
Laurens Van Houtven <_@lvh.io>
2016-01-21 17:58:27 -0600

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/RackSec/unclogged"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC5"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.cloudbees/syslog-java-client "1.0.8"]
-                 [manifold "0.1.2-alpha2"]
+                 [manifold "0.1.2-alpha3"]
                  [com.taoensso/timbre "4.2.0"]]
   :plugins [[lein-cljfmt "0.3.0"]
             [lein-cloverage "1.0.7-SNAPSHOT"]]
