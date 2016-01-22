493e4107748226e3b2b03f7ed7ca6c94f20586bc
Laurens Van Houtven <_@lvh.io>
2016-01-21 18:22:25 -0600

    At some point I will learn to type my pin correctly

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject unclogged "0.6.0"
+(defproject unclogged "0.6.0-SNAPSHOT"
   :description "Clojure syslog abstraction"
   :url "https://github.com/RackSec/unclogged"
   :license {:name "Eclipse Public License"
